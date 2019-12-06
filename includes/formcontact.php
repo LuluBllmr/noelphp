6e14217626ed25d56642da4e2484e0aa505b0bc2
Lucas LABATUT <lucas.labatut@outlook.fr>
2019-12-06 14:50:14 +0100

    cours php

---
+++ includes/formcontact.php
@@ -0,0 +1,34 @@
+<form action="#" method="post">
+  <div>
+
+    <label for="nom">Nom:</label>
+    <input type="text" name="nom" id="nom"/>
+
+  </div>
+  <div>
+
+    <label for="prenom">Prénom:</label>
+    <input type="text" name="prenom" id="prenom"/>
+
+  </div>
+  <div>
+
+    <label for="email">E-Mail:</label>
+    <input type="email" name="email" id="email"/>
+
+  </div>
+  <div>
+
+    <label for="mdp">Mot de passe:</label>
+    <input type="password" name="mdp" id="mdp"/>
+
+  </div>
+    <input type="submit" value="Envoyer">
+    <div>
+
+      <label for="caché">Champs caché:</label>
+      <input type="hidden" name="caché" id="caché"/>
+
+    </div>
+      <input type="submit" value="Envoyer">
+</form>
---
+++ includes/pages/contact.inc.php
@@ -0,0 +1,17 @@
+<?php
+
+require './includes/formcontact.php';
+
+
+?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>eryu-èu</title>
+    <link rel="stylesheet" href="./assets/css/style.css">
+  </head>
+  <body>
+
+  </body>
+</html>
